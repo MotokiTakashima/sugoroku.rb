70b04697fcb47e58ac5986536c1523fe4ab57b50
motokitakashima <pageboy.jr@icloud.com>
2021-04-30 15:09:04 +0900

    playerをsugoroku,rbに移動

--- sugoroku.rb
+++ sugoroku.rb
@@ -11,25 +11,26 @@ class Sugoroku
     build_map
     build_dice
     build_character
+    build_player
 
     informaition
 
-    while player.position < @map.max_square && computer.position < @map.max_square
+    while @player.position < @map.max_square && computer.position < @map.max_square
       #プレイヤーのサイコロを振らす処理
-      player.roll_dice
+      @player.roll_dice
       #サイコロの出目の表示
-      @dice.disp_dice(player)
+      @dice.disp_dice(@player)
       #プレイヤーの進んだ位置を表示
-      player.move_forward(@map)
+      @player.move_forward(@map)
       #プレイヤーが止まったマスのギミック
-      @map.activate_gimmick(player)
+      @map.activate_gimmick(@player)
       #プレイヤーの現在地を表示、ゴール判定
-      player.show_square(@map)
-      if player.position == @map.max_square
+      @player.show_square(@map)
+      if @player.position == @map.max_square
         break
       end
       #CPUがサイコロを振り、進んだマスを表示
-      computer.roll_dice(player, @map, @dice, computer)
+      computer.roll_dice(@player, @map, @dice, computer)
       #CPUが止まったマスのギミック
       @map.activate_gimmick(computer)
       #CPUの現在地を表示、ゴール判定
@@ -60,5 +61,9 @@ class Sugoroku
 
   def build_character
     @character = Character.new(0, 0)
+  end
+
+  def build_player
+    @player = Player.new(0, 0)
   end
 end
