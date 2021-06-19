e83561d004caa69e9c20b731fcf6cff390ef2fc6
motokitakashima <pageboy.jr@icloud.com>
2021-06-19 14:51:06 +0900

    change_position を移動

--- map.rb
+++ map.rb
@@ -3,6 +3,19 @@ require_relative "character"
 class Map
   attr_reader :max_square
 
+  @gimmick = [{ square: 1, position: 2 },
+              { square: 3, position: -2 },
+              { square: 5, position: -5 },
+              { square: 7, position: -3 },
+              { square: 10, position: 5 },
+              { square: 14, position: -7 },
+              { square: 17, position: -3 },
+              { square: 20, position: -20 },
+              { square: 26, position: -26 },
+              { square: 28, position: -10 },
+              { square: 29, position: -29 },
+            ]
+
   def initialize
     @max_square = 30
   end
--- sugoroku.rb
+++ sugoroku.rb
@@ -48,6 +48,7 @@ class Sugoroku
            ゴールまで#{@map.max_square}マスです
          text
   end
+
   
   def move_forward(character)
     puts <<~text
@@ -66,7 +67,7 @@ class Sugoroku
   
   def show_square(character)
     if character.position < @map.max_square
-      character.change_position
+      change_position(character)
       puts <<~text
       現在#{character.name}は、#{character.position}マス目です。
       
@@ -93,7 +94,7 @@ class Sugoroku
     puts ""
     puts "-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:"
   end
-
+  
   def play_user(character)
     character.roll_dice(@dice)
     move_forward(character)
@@ -104,4 +105,30 @@ class Sugoroku
     show_square(character)
   end
   
+  def change_position(character)
+    case character.position
+    when 1
+      character.position += 2
+    when 3
+      character.position -= 2
+    when 5
+      character.position -= 5
+    when 7
+      character.position += 2
+    when 10
+      character.position += 5
+    when 14
+      character.position -= 7
+    when 17
+      character.position -= 3
+    when 20
+      character.position -= 20
+    when 26
+      character.position -= 26
+    when 28
+      character.position -= 10
+    when 29
+      character.position -= 29
+    end
+  end
 end
